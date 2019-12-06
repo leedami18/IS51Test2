bf8b9072c2fd3c47d77cd7c69f8618e155415f96
Damian Lee <dlee18@mail.fresnostate.edu>
2019-12-05 17:42:28 -0800

    10 min

--- src/app/test-score/test-score.component.ts
+++ src/app/test-score/test-score.component.ts
@@ -4,6 +4,14 @@ import { ActivatedRoute } from '@angular/router';
 import { Router } from '@angular/router';
 import { ToastService } from '../toast/toast.service';
 
+export interface ITest {
+  id?: number;
+  testName: string;
+  pointsPossible: number;
+  pointsReceived: number;
+  percentage: number;
+  grade: string;
+}
 
 @Component({
   selector: 'app-test-score',
@@ -12,7 +20,9 @@ import { ToastService } from '../toast/toast.service';
 })
 export class TestScoreComponent implements OnInit {
 
-  tests: Array<any> = [];
+  tests: Array<ITest> = [];
+  nameInput = '';
+
   constructor(
     private http: Http,
     private activatedRoute: ActivatedRoute,
@@ -21,6 +31,70 @@ export class TestScoreComponent implements OnInit {
   ) { }
 
   async ngOnInit() {
+    this.tests = await this.loadTests();
+  }
+
+  async loadTests() {
+    let tests = JSON.parse(localStorage.getItem('tests'));
+    if (tests && tests.length > 0) {
+
+    } else {
+      tests = await this.loadTestsFromJson();
+    }
+    this.tests = tests;
+    return tests;
+  }
+
+  async loadTestsFromJson() {
+    const tests = await this.http.get('assets/tests.json').toPromise();
+    return tests.json();
+  }
+
+  addTest() {
+    const test: ITest = {
+      id: null,
+      testName: null,
+      pointsPossible: null,
+      pointsReceived: null,
+      percentage: null,
+      grade: null
+    };
+    this.tests.push(test);
+  }
+
+  saveToLocalStorage() {
+    localStorage.setItem('tests', JSON.stringify(this.tests));
+    this.toastService.showToast('success', 3000, 'Success: Items saved!');
+  }
+
+  deleteTest() {
+    this.tests.splice(-1, 1);
+    this.saveToLocalStorage();
+    // fix validation
+  }
+  computeGrade() {
+    console.log('nameInput', this.nameInput);
+
+    if (this.nameInput === '') {
+      this.toastService.showToast('warning', 5000, 'name must not be null');
+    } else if (this.nameInput.indexOf(',') === -1) {
+      this.toastService.showToast('warning', 5000, 'name must have a ,');
+    } else {
+      const data = this.calculate();
+      this.router.navigate(['home', data]);
+    }
+  }
+
+  calculate() {
+    let totals = 0, earned = 0;
+    for (let i = 0; i < this.tests.length; i++) {
+      totals += this.tests[i].pointsPossible;
+      earned += this.tests[i].pointsReceived;
+    }
+    return {
+      totalPoints: totals,
+      totalRecieved: earned,
+    };
 
   }
 
